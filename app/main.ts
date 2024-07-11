dda8700ec8392beeaa9e0a5195605a70a636127e
KJAVA19811815 <karunsingh1993@gmail.com>
2024-07-11 00:24:12 -0400

    sending 200 response

--- app/main.ts
+++ app/main.ts
@@ -3,11 +3,17 @@ import * as net from "net";
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  socket.on("data", (data) => {
+    console.log('data from socket inbound', data.toString());
+    socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
+  })
   socket.on("close", () => {
+    console.log('closing connection')
     socket.end();
   });
 });
 
-server.listen(4221, "localhost");
+server.listen(4221, "localhost", () => {
+  console.log("Server is listening on localhost:4221");
+});

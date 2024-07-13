f3c01e1be5da8b8dbafc6f9bbbccd6e9cd35234f
KJAVA19811815 <karunsingh1993@gmail.com>
2024-07-12 23:45:11 -0400

    read header

--- app/main.ts
+++ app/main.ts
@@ -1,16 +1,17 @@
 import * as net from "net";
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    const [method, path, version] = data.toString().split(' ');
-    console.log('PATH', path, path.includes('echo'), path.split('/'));
+    console.log('data received', data.toString())
+    const inputData = data.toString().split(' ');
+    console.log('inputData', inputData)
+    const path = inputData[1];
+    const userAgent = inputData[5];
+    const accpetableRoutes = ['/', '/echo', 'user-agent'];
     const splitPath = path.split('/')
     const route = splitPath[splitPath.length - 1]
-    if (path.includes('echo') || path === '/') {
-      socket.write(Buffer.from(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${route.length}\r\n\r\n${route}`));
+    if (accpetableRoutes.includes(route)) {
+      socket.write(Buffer.from(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.trim().length}\r\n\r\n${userAgent}`));
       return;
     } else {
       socket.write(Buffer.from(`HTTP/1.1 404 Not Found\r\n\r\n`));
